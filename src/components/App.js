23204afdd4633b07dc22d0c7190ed79841ca5bd3
Srijan <srijankarki0@gmail.com>
2023-12-15 13:25:10 +0545

    mapping buttons and components

--- src/components/App.js
+++ src/components/App.js
@@ -8,33 +8,46 @@ import CashFlowCalculator from "./CashFlowCalculator";
 import { useState } from "react";
 import BillingList from "./BillingLists";
 import { STOCKS } from "./constants";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [whatToShow, setWhatToShow] = useState("");
   const [stocks, setStocks] = useState(STOCKS);
 
-  // const BUTTON = [
-  //   {
-  //     title: "First Component",
-  //     id: "firstcomponent-btn",
-  //     field: "firstcomponent",
-  //   },
-  //   {
-  //     title: "Nepse Stocks",
-  //     id: "nepsestocks-btn",
-  //     field: "nepsestocks",
-  //   },
-  //   {
-  //     title: "Cash Flow Calculator",
-  //     id: "cashflowcalculator-btn",
-  //     field: "cashflowcalculator",
-  //   },
-  //   {
-  //     title: "Billing List",
-  //     id: "billinglist-btn",
-  //     field: "billinglist",
-  //   },
-  // ];
+  const BUTTONS = [
+    {
+      title: "First Component",
+      key: "first",
+      component: (
+        <FirstComponent
+          name="Test"
+          courseName={"MERN"}
+          age={23}
+          isStudent={true}
+          //objects
+          details={{ address: "Kathmandu", bloodGroup: "O +" }}
+          //array
+          subjects={["mongodb", "ExpressJS", "ReactJS", "NodeJS"]}
+          square={(x) => x * x}
+        />
+      ),
+    },
+    {
+      title: "Nepse Stocks",
+      key: "nepseStocks",
+      component: <NepseStocks setStocks={setStocks} stocks={stocks} />,
+    },
+    {
+      title: "Cash Flow Calculator",
+      key: "cashFlow",
+      component: <CashFlowCalculator />,
+    },
+    {
+      title: "Billing List",
+      key: "billinglist",
+      component: <BillingList stocks={stocks} />,
+    },
+  ];
 
   return (
     <div className="App">
@@ -47,24 +60,30 @@ function App() {
           value={whatToShow}
           onChange={(e) => setWhatToShow(e.target.value)}
         />
-
-        {/* {BUTTON.map((a) => (
-          <button id={a.id} key={a.id} onclick={(e) => setWhatToShow(a.field)}>
-            {a.title}
-          </button>
-        ))} */}
+        <span>
+          {BUTTONS.map((a) => (
+            <button
+              key={a.key}
+              onclick={(e) => setWhatToShow(a.key)}
+              className={a.key === whatToShow ? "selected" : ""}
+            >
+              {a.title}
+            </button>
+          ))}
+        </span>
+        {BUTTONS.map((v) => v.key === whatToShow && v.component)}
         {/* <button onClick={(e) => setWhatToShow("cashFlow")}>
           Cash Flow Calculator
-          </button>          
-          <button onClick={(e) => setWhatToShow("nepseStocks")}>
+        </button>
+        <button onClick={(e) => setWhatToShow("nepseStocks")}>
           Nepse Stock
-          </button>          
-          <button onClick={(e) => setWhatToShow("billinglist")}>
+        </button>
+        <button onClick={(e) => setWhatToShow("billinglist")}>
           Billing List
         </button> */}
 
         {/* <button onClick={(e) => setWhatToShow("first")}>First Component</button> */}
-        {whatToShow === "first" && (
+        {/* {whatToShow === "first" && (
           <FirstComponent
             name="Test"
             courseName={"MERN"}
@@ -81,13 +100,13 @@ function App() {
         {whatToShow === "nepseStocks" && (
           <NepseStocks
             setWhatToShow={setWhatToShow}
-            // BUTTON={BUTTON}
+            BUTTONS={BUTTONS}
             setStocks={setStocks}
             stocks={stocks}
           />
         )}
 
-        {whatToShow === "billinglist" && <BillingList stocks={stocks} />}
+        {whatToShow === "billinglist" && <BillingList stocks={stocks} />} */}
 
         <ToastContainer />
         {/* <SecondComponent /> */}
