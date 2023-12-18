b228957fbf5b9c3fc15d2937d9020845e71e6fc7
Srijan <srijankarki0@gmail.com>
2023-12-18 15:18:18 +0545

    created sample reducer for useReducer

--- src/contexts/SampleContext.js
+++ src/contexts/SampleContext.js
@@ -1,6 +1,20 @@
-import { createContext } from "react";
+import { createContext, useDeferredValue, useReducer } from "react";
 export const SampleContext = createContext();
+
+const sampleReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_TEST":
+      state.test = action.payload;
+      return state;
+    default:
+      return state;
+  }
+};
+
 const SampleProvider = ({ childern }) => {
+  const [sampleState, dispatch] = useReducer(sampleReducer, {
+    test: "Sample Text",
+  });
   return (
     <SampleContext.Provider value={{ test: "Sample text" }}>
       {childern}
