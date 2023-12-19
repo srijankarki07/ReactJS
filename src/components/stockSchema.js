9f60c93cade37755e66a27d59ee0299d1911206e
Srijan <srijankarki0@gmail.com>
2023-12-19 20:04:37 +0545

    formik validation of schema

--- src/components/stockSchema.js
+++ src/components/stockSchema.js
@@ -1,9 +1,14 @@
 import { object, string, number, date, InferType } from "yup";
 
 export const stockSchema = object({
-  securityName: string().required(),
-  securityId: number().required().positive().integer(),
-  symbol: string().required(),
-  totalTradedQuantity: number().required(),
+  securityName: string("Name is Required").required("Can't be empty"),
+  securityId: number()
+    .required("Mandatory")
+    .positive("Must be postitive")
+    .integer("Must not be decimal"),
+  symbol: string("Symbol can only be alphabets")
+    .required("Can't be empty")
+    .min(3, "Required atleast 3"),
+  // totalTradedQuantity: number().required(),
   // lastTradedPrice: number(),required().positive(),
 });
